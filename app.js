9e9813f7942c966db59c2f97581db6c377ed5ae0
Brian Tovar <brian.tovar@gmail.com>
2016-02-13 16:33:18 -0500

    about to use heroku

--- app.js
+++ app.js
@@ -3,11 +3,31 @@ var app = express();
 
 app.set('view engine', 'jade');
 app.set('port', (process.env.PORT || 3000));
-//app.use(express.static('public'));
+app.use(express.static('public'));
 app.use(express.static('public/bower_components'));
 
 app.get('/', function(request, response) {
-  response.render('index');
+  response.render('home');
+});
+
+app.get('/projects', function(request, response) {
+  response.render('projects');
+});
+
+app.get('/projects/:id', function(request, response) {
+  if ( request.params.id == 'chat') {
+    response.render('chat');
+  } else {
+    response.sendStatus(404);
+  }
+});
+
+app.get('/resume', function(request, response) {
+  response.render('resume');
+});
+
+app.get('/about', function(request, response) {
+  response.redirect('/about');
 });
 
 app.listen(app.get('port'), function() {
